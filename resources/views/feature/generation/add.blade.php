7b054dba14ddb76afc3059855105abd64b1305ae
Met Chanthou <met.chanthou@optistech.com>
2025-06-07 12:24:34 +0700

    fixe col-card

--- resources/views/feature/generation/add.blade.php
+++ resources/views/feature/generation/add.blade.php
@@ -23,7 +23,7 @@
             {{-- Terms card --}}
             <div class="row justify-content-center">
                 <div class="col-sm-6 col-md-4 col-xl-3">
-                    <div class="card" style="width: 18rem; height: 10rem;">
+                    <div class="card" style="height: 10rem;">
                         <div class="card-header dropstart">
                             <button class="btn-term" data-bs-toggle="dropdown">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
@@ -42,7 +42,7 @@
                     </div>
                 </div>
                 <div class="col-sm-6 col-md-4 col-xl-3">
-                    <div class="card" style="width: 18rem; height: 10rem;">
+                    <div class="card" style="height: 10rem;">
                         <div class="card-header">
                             <button class="btn-t btn-term">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
@@ -56,7 +56,7 @@
                     </div>
                 </div>
                 <div class="col-sm-6 col-md-4 col-xl-3">
-                    <div class="card" style="width: 18rem; height: 10rem;">
+                    <div class="card" style="height: 10rem;">
                         <div class="card-header">
                             <button class="btn-t btn-term">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
@@ -70,7 +70,7 @@
                     </div>
                 </div>
                 <div class="col-sm-6 col-md-4 col-xl-3">
-                    <div class="card" style="width: 18rem; height: 10rem;">
+                    <div class="card" style="height: 10rem;">
                         <div class="card-header">
                             <button class="btn-t btn-term">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
@@ -83,10 +83,10 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-sm-6 col-md-3">
-                    <div class="card" style="width: 18rem; height: 10rem;">
-                        <div class="card-body d-flex justify-content-center align-items-center">
-                            <h5 class="card-title" style="color: black;">
+                <div class="col-sm-6 col-md-4 col-xl-3">
+                    <div class="card" style="height: 10rem;">
+                        <div class="card-body card-add d-flex justify-content-center align-items-center">
+                            <h5 class="card-title add" style="color: black;">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
                                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
                                     <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
