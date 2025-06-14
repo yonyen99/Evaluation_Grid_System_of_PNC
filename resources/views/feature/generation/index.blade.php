9dc5851fa20f3202b988d2839ef1c0dbcf6208c9
Yen Yon <yonyen99@gmail.com>
2025-06-14 11:09:38 +0700

    improve delete

--- resources/views/feature/generation/index.blade.php
+++ resources/views/feature/generation/index.blade.php
@@ -3,27 +3,29 @@
 @section('stylesheet')
     <link href="{{ asset('dashboard/css/generation.css') }}" rel="stylesheet" />
 @endsection
+
 {{-- BEGIN:: Table Content --}}
 @section('content')
     <div class="row">
         <div class="col-md-12 d-flex justify-content-between">
             <h4 class="title"> Generation List</h4>
-            <button type="button" class="btn btn-outline-primary d-flex align-items-center">
+            <a href="{{ route('generation-add') }}" class="btn btn-outline-primary d-flex align-items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                     class="bi bi-plus-circle-fill me-2" viewBox="0 0 16 16">
                     <path
                         d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
                 </svg>
-                <a href="{{ route('generation-add') }}">New Generate</a>
-            </button>
+                New Generate
+            </a>
         </div>
+
         <!-- Table List -->
-        <div class="col-md-12">
-            <div class="card ">
+        <div class="col-md-12 mt-3">
+            <div class="card">
                 <div class="card-body">
                     <div class="table-responsive" style="padding-bottom: 0 !important">
-                        <table class="table tablesorter " id="">
-                            <thead class=" text-primary">
+                        <table class="table tablesorter">
+                            <thead class="text-primary">
                                 <tr>
                                     <th class="text-left">No</th>
                                     <th class="text-center">Generation</th>
@@ -36,17 +38,17 @@
                                     <tr>
                                         <td class="text-left">{{ $index + 1 }}</td>
                                         <td class="text-center">{{ $generation->name }}</td>
-                                        <td class="d-flex justify-center text-center ">
+                                        <td class="d-flex justify-center text-center">
                                             @foreach ($generation->terms as $term)
                                                 <div class="m-1 text-center">{{ $term->name }} | </div>
                                             @endforeach
                                         </td>
-                                        <!-- Action -->
                                         <td class="text-right">
-                                            <form class="btn btn-sm btn-danger" action="{{ route('generation-delete', ['id' => $generation->id]) }}" method="POST" >
-                                                @method('delete')
+                                            {{-- Delete Button --}}
+                                            <form class="d-inline delete-form" method="POST" action="{{ route('generation-delete', ['id' => $generation->id]) }}">
                                                 @csrf
-                                                <button class="btn-danger">
+                                                @method('delete')
+                                                <button type="submit" class="btn btn-sm btn-danger">
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
                                                         fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                                                         <path
@@ -55,8 +57,9 @@
                                                 </button>
                                             </form>
 
+                                            {{-- Edit Button --}}
                                             <a class="btn btn-sm btn-warning" href="{{ url("generation/$generation->id/edit") }}">
-                                                <button type="button" class="btn-warning" click="">
+                                                <button type="button" class="btn-warning">
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
                                                         fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                                                         <path
@@ -66,8 +69,10 @@
                                                     </svg>
                                                 </button>
                                             </a>
-                                            <a class="btn btn-sm btn-info" href="">
-                                                <button type="button" class="btn-info" click="">
+
+                                            {{-- View Button --}}
+                                            <a class="btn btn-sm btn-info" href="#">
+                                                <button type="button" class="btn-info">
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
                                                         fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
                                                         <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0" />
@@ -86,53 +91,21 @@
             </div>
         </div>
     </div>
-
-    </div>
 @endsection
 {{-- END:: Table Content --}}
 
-{{-- custom script --}}
+{{-- Custom Script --}}
 @section('script')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="{{ asset('dashboard/js/generation.js') }}"></script>
     <script>
-              
-        $(document).ready(function(){
-            // show confirm delete message
-            $('.btn-danger').on('click', function(){
-                return confirm('Do you really want to delete this Generation record?');
+        // One-click confirm before form submit
+        document.querySelectorAll('.delete-form').forEach(function (form) {
+            form.addEventListener('submit', function (e) {
+                if (!confirm('Do you really want to delete this Generation record?')) {
+                    e.preventDefault();
+                }
             });
-            
         });
-        // $(document).ready(function() {
-        //     // CSRF token setup for Laravel
-        //     $.ajaxSetup({
-        //         headers: {
-        //             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        //         }
-        //     });
-
-        //     // Handle Save button
-        //     $('#saveGenerate').on('click', function() {
-        //         const formData = {
-        //             name: $('#generationName').val()
-        //         };
-
-        //         $.ajax({
-        //             url: '/generations', // Laravel route to store
-        //             type: 'POST',
-        //             data: formData,
-        //             success: function(response) {
-        //                 $('#exampleModal').modal('hide');
-        //                 $('#generateForm')[0].reset();
-        //                 alert('Generation Created!');
-        //                 // You can also call a function to reload the list
-        //             },
-        //             error: function(xhr) {
-        //                 alert('Failed to create. Check input or server.');
-        //             }
-        //         });
-        //     });
-        // });
     </script>
 @endsection
