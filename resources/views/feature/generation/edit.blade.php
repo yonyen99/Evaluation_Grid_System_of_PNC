8f1679f72fc55cd72a0f2e8a530ad606f6eb7b21
yen yon <yonyen99@gmail.com>
2025-07-26 09:54:22 +0700

    imp

--- resources/views/feature/generation/edit.blade.php
+++ resources/views/feature/generation/edit.blade.php
@@ -2,7 +2,7 @@
 @section('page_title', 'Edit Generation ')
 
 @section('stylesheet')
-    <link href="{{ asset('dashboard/css/generation.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/generation.css') }}" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 @endsection
