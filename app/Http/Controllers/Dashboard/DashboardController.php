986ddff9234827012899ad37fee4339c5a47e08e
somounom123 <somounom077@gmail.com>
2025-08-15 15:21:11 +0700

    count student class teacher recode current year

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\Classe;
+use App\Models\Student;
+use App\Models\Teacher;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -13,6 +17,16 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('index');
+        $currentYear = Carbon::now()->year;
+        // Get total students
+        $students = Student::whereYear('created_at', $currentYear)->get();
+        $totalStudentCurrentYears  = count($students);
+        // Get total teachers
+        $teachers  = Teacher::whereYear('created_at', $currentYear)->get();
+        $totalTeacherCurrentYears  = count($teachers);
+        // Get total Classes 
+        $classes  = Classe::whereYear('created_at', $currentYear)->get();
+        $totalClassesCurrentYears  = count($classes);
+        return view('index',compact('totalStudentCurrentYears','totalTeacherCurrentYears','totalClassesCurrentYears'));
     }
 }
